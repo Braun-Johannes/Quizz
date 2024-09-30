ebc270e3e9de567a04c7d513701d6509c5705f7a
Braun-Johannes <Braun-Johannes@outlook.de>
2024-09-30 11:50:23 +0200

    cleaned template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>Home</h1>
+    <p>Welcome to the home page</p>
+  </div>
 </template>
+
+<style>
+.container {
+  border: white solid 1px;
+  border-radius: 15px;
+
+  max-width: 80%;
+  margin: 0 auto;
+  padding: 1rem;
+}
+@media (min-width: 1024px) {
+  .container {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
