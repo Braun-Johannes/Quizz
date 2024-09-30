9a3dc07abb32c15ac4dc730c8c9760b615dd2c66
Braun-Johannes <Braun-Johannes@outlook.de>
2024-09-30 14:17:26 +0200

    added function to switch between play create and library

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,52 @@
+<script>
+export default {
+  data() {
+    return {
+      currentSection: 'play'
+    }
+  },
+  methods: {
+    showSection(section) {
+      this.currentSection = section
+    }
+  }
+}
+</script>
+
 <template>
-  <div class="container">
-    <span class="dot"></span>
+  <div class="play" v-show="currentSection === 'play'">
+    <span class="logoDot"></span>
     <p class="slogan">Show them your Quizz</p>
+  </div>
+
+  <div class="create" v-show="currentSection === 'create'">
+    <span class="logoDot"></span>
+    <p class="slogan">Show them a different Quizz</p>
+  </div>
+
+  <div class="library" v-show="currentSection === 'library'">
+    <span class="logoDot"></span>
+    <p class="slogan">Show them your last Quizz</p>
+  </div>
+
+  <div class="selected">
+    <span class="dot" @click="showSection('play')" id="dot1"></span>
+    <span class="dot" @click="showSection('create')" id="dot2"></span>
+    <span class="dot" @click="showSection('library')" id="dot2"></span>
   </div>
 </template>
 
 <style>
-.dot {
-  height: 25px;
-  width: 25px;
+.logoDot {
+  height: 100px;
+  width: 100px;
   background-color: #2e6464;
   border-radius: 50%;
   display: inline-block;
   text-align: center;
   margin: auto;
 }
-/* .dot later gets the icon/play button*/
+/* .logoDot later gets the icon/play button*/
 
 .slogan {
   font-size: 1.5rem;
@@ -24,7 +55,9 @@
   padding: 1rem;
 }
 
-.container {
+.play,
+.create,
+.library {
   border: white solid 1px;
   border-radius: 15px;
 
@@ -37,8 +70,43 @@
   flex-direction: column;
   justify-content: flex-end;
 
-  
+  transition: all 0.6s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  /* transition for swipe function */
 }
+
+.play.previous,
+.create.previous,
+.library.previous {
+  transform: translateX(-100%);
+}
+.play.active,
+.create.active,
+.library.active {
+  transform: translateX(0%);
+}
+.play.next,
+.create.next,
+.library.next {
+  transform: translateX(-100%);
+}
+
+.selected {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+}
+
+.dot {
+  height: 15px;
+  width: 15px;
+  background-color: #ffffff;
+  border-radius: 50%;
+  display: inline-block;
+  text-align: center;
+  margin: 5px;
+}
+
 @media (min-width: 1024px) {
   .container {
     display: flex;
