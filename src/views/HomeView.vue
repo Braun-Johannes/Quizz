a0f886064a9f009f4c9b28fc84c37e467a6f19fb
Braun-Johannes <Braun-Johannes@outlook.de>
2024-09-30 12:15:11 +0200

    filled first template and adjusted

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="container">
-    <h1>Home</h1>
-    <p>Welcome to the home page</p>
+    <span class="dot"></span>
+    <p class="slogan">Show them your Quizz</p>
   </div>
 </template>
 
 <style>
+.dot {
+  height: 25px;
+  width: 25px;
+  background-color: #2e6464;
+  border-radius: 50%;
+  display: inline-block;
+  text-align: center;
+  margin: auto;
+}
+/* .dot later gets the icon/play button*/
+
+.slogan {
+  font-size: 1.5rem;
+  color: #fff;
+  text-align: center;
+  padding: 1rem;
+}
+
 .container {
   border: white solid 1px;
   border-radius: 15px;
 
   max-width: 80%;
+  min-height: 80vh;
   margin: 0 auto;
   padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+
+  
 }
 @media (min-width: 1024px) {
   .container {
